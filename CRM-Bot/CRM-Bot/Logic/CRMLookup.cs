7f59716a116bd63c4c75b7de3bed36283d085e4a
Vishesh Oberoi <o.vishesh@outlook.com>
2016-10-19 17:27:50 +1300

    crm lookup added

---
+++ CRM-Bot/CRM-Bot/Logic/CRMLookup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRM_Bot.Logic
+{
+    public class CRMLookup
+    {
+        public string CRMLookupDialogue(string tag)
+        {
+            switch (tag)
+            {
+                case "Meeting Minutes":
+                    return "";
+
+                case "Last Order":
+                    return "";
+
+                case "Any Dues":
+                    return "";
+
+                case "Key Contacts":
+                    return "";
+
+                default:
+                    return "";
+            }
+        }
+    }
+}
